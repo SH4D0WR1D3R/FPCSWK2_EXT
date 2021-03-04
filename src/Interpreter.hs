f27e3f971d50a81a1035328c05e159f5a474e536
SH4D0WR1D3R <anothersh4d0wr1d3r@gmail.com>
2021-03-04 00:25:18 +0000

    Assign done

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -29,35 +29,105 @@ data Err
 -- what evaluating the program does to the memory.
 interpret :: Program -> Memory -> Either Err Memory
 interpret [] mem = Right mem
-interpret (p:ps) mem = undefined
+interpret (p:ps) mem = current p mem >>= interpret ps 
 
 current :: Stmt -> Memory -> Either Err Memory -- evaluates the current statement - pattern matches for different statement types
-current (AssignStmt var ex) mem = undefined
+--current [] mem = Right mem
+current (AssignStmt var ex) mem = editMemory var ex mem-- UNSURE
+--current (IfStmt x (t:ts) [ex, (e:es)] (f:fs)) mem = eval a >>= \b -> if b /= 0 then eval t else (eval ex >>= \c -> if c /= 0 then eval e else eval f)
 --current (IfStmt Expr [Stmt] [(Expr, [Stmt])] [Stmt]) mem = undefined 
 --current (RepeatStmt Expr [Stmt]) mem = undefined
+
+editMemory :: String -> Expr -> Memory -> Either Err Memory
+editMemory s x mem = eval x mem >>= \val -> case lookup s mem of -- BINDING GETS RID OF EITHER
+     Nothing -> Right $ (s, val):mem
+     Just _ -> Right $ replaceMemory s val mem
+
+replaceMemory :: String -> Int -> Memory -> Memory
+replaceMemory _ _ [] = []
+replaceMemory s i (m:ms)
+    | fst m == s = (s, i):ms
+    | otherwise = m:(replaceMemory s i ms)
 
 {-myLookup :: Expr -> Memory -> Either Err Int
 myLookup (VarE x) mem = case lookup x mem of 
      Nothing -> Left UninitialisedMemory x
      Just y -> Right y-}
 
+safediv :: Int -> Int -> Either Err Int
+safediv _ 0 = Left DivByZeroError
+safediv x y = Right $ x `div` y
+
+-- WANT LOOKUP TO WORK ON EITHER TYPE TO MAKE IT WORK FOR MEMORY
+
 eval :: Expr -> Memory -> Either Err Int
 eval (ValE x) _ = Right x
 eval (VarE x) mem = case lookup x mem of 
      Nothing -> Left $ UninitialisedMemory x
      Just y -> Right y
-eval (BinOpE Add x y) mem = do
-     x <- eval x mem
-     y <- eval y mem
-     return Right $ x + y
+eval (BinOpE Add a b) mem = do
+     x <- eval a mem
+     y <- eval b mem
+     return $ x + y -- SPLIT UP BIN OP PATTERN MATCHING TO SEPARATE FUNCTION
+eval (BinOpE Sub a b) mem = do
+     x <- eval a mem
+     y <- eval b mem
+     return $ x - y
+eval (BinOpE Mul a b) mem = do
+     x <- eval a mem
+     y <- eval b mem
+     return $ x * y
+eval (BinOpE Div a b) mem = do
+     x <- eval a mem
+     y <- eval b mem
+     x `safediv` y
+eval (BinOpE Pow a b) mem = do
+     x <- eval a mem
+     y <- eval b mem
+     return $ x ^ y
+eval (BinOpE Equal a b) mem = do
+     x <- eval a mem
+     y <- eval b mem
+     return $ (if x == y then 1 else 0)
+eval (BinOpE Neq a b) mem = do
+     x <- eval a mem
+     y <- eval b mem
+     return $ (if x /= y then 1 else 0)
+eval (BinOpE LessThan  a b) mem = do
+     x <- eval a mem
+     y <- eval b mem
+     return $ (if x < y then 1 else 0)
+eval (BinOpE LessOrEqual a b) mem = do
+     x <- eval a mem
+     y <- eval b mem
+     return $ (if x <= y then 1 else 0)
+eval (BinOpE GreaterThan  a b) mem = do
+     x <- eval a mem
+     y <- eval b mem
+     return $ (if x > y then 1 else 0)
+eval (BinOpE GreaterOrEqual a b) mem = do
+     x <- eval a mem
+     y <- eval b mem
+     return $ (if x >= y then 1 else 0)
+
 
 -- FUNCTION TO CONVERT AN EITHER TO JUST AN INT
+
+--evalBin :: Op -> Expr -> Expr 
+--evalBin 
+
+
+
+
+
+
+
 
 {-eval :: Expr -> Memory -> Maybe Int
 eval (ValE x) _ = Just x
 --evalBin (VarE x) (m:ms) = if fst m == then snd m else evalBin (VarE x) ms
 eval (VarE x) mem = lookup x mem
-eval (BinOpE o x y) mem = Just (operation o (eval x mem) (eval y mem))
+eval (BinOpE o a b) mem = Just (operation o (eval a mem) (eval b mem))
 
 operation :: Op -> Maybe Int -> Maybe Int -> Int
 operation Add (Just x) (Just y) = x + y
